6aa3e4fb838195eade8e2052af5b323da5aaa510
max2079 <a112221041@mail.shu.edu.tw>
2024-09-20 12:01:25 +0800

    113-09-20

--- 113-09-20/helloworld.java
+++ 113-09-20/helloworld.java
@@ -1,5 +1,5 @@
-public class helloworld {
-    public static void main(String[] arg){
-        System.out.println("Hello world");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
