63571df57849165af46ac3f266abf5674439a173
max2079 <a112221041@mail.shu.edu.tw>
2024-11-29 11:55:22 +0800

    Add files via upload

---
+++ 113-11-29/InnerClass/src/CalculatorTest.java
@@ -0,0 +1,9 @@
+public class CalculatorTest {
+    public static void main(String[] args){
+        Calculator calculator = new Calculator();
+        calculator.perform0perations(10, "+", 5);
+        calculator.perform0perations(10, "-", 5);
+        calculator.perform0perations(10, "+", 5);
+        calculator.perform0perations(10, "/", 5);
+    }
+}
